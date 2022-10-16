02b46750a456e79c840fe4b2a90946a646b734aa
FelipeOthon <felipeosantiago@gmail.com>
2022-10-16 14:26:55 -0300

    enviar class java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite um numero");
+        int numero = scanner.nextInt();
+
+        if (numero < 0)
+
+            System.out.println("Numero Negativo");
+
+        else
+
+            System.out.println("Numero Positivo");
+
+
+        scanner.close();
     }
 }
